d56f5ad2a19cb35b64035e81bf00ed301a95a45b
helenjim49 <helenjim49@gmail.com>
2022-06-21 22:15:06 +0200

    init

--- test/de/tum/in/ase/eist/DiscussionTest.java
+++ test/de/tum/in/ase/eist/DiscussionTest.java
@@ -13,5 +13,39 @@ import static org.junit.jupiter.api.Assertions.*;
 class DiscussionTest {
 
     // TODO implement the tests
+    @TestSubject
+    Discussion Discussion = new Discussion();
 
+    @Mock
+    Comment commentMock;
+    Course courseMock;
+
+    @Test
+    void testComment() {
+        int expectedNumberOfComments = Discussion.getNumberOfComments() + 1;
+        expect(commentMock.save()).andReturn(true);
+        replay(courseMock);
+        Discussion.addComment(commentMock);
+        assertEquals(expectedNumberOfComments, Discussion.getNumberOfComments());
+    }
+
+    @Test
+    void testCommentIfSavingFails() {
+        int expectedNumberOfComments = Discussion.getNumberOfComments();
+        expect(commentMock.save()).andReturn(false);
+        replay(courseMock);
+        Discussion.addComment(commentMock);
+        assertEquals(expectedNumberOfComments, Discussion.getNumberOfComments());
+    }
+
+    @Test
+    void testStartCourseDiscussion() {
+        Student shawn = new Student("Shawn", "Mendes", LocalDate.parse("1998-08-08"), "Music", "Portuguese");
+        expect(courseMock.isDiscussionAllowed(shawn)).andReturn(true);
+        replay(courseMock);
+        assertTrue(Discussion.startCourseDiscussion(courseMock, shawn, "Wonder"));
+        String expected_topic = "Wonder";
+        String observed_topic = Discussion.getTopic();
+        assertEquals(expected_topic, observed_topic);
+    }
 }
